0fa10d680f52bed7d07cc9b8b0d4d34331d1368a
masterTony <mastertony04@gmail.com>
2022-03-07 08:49:47 +0300

    added all codes but untested to the incubator

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ SimpleTimer demo(180000);
 
 SoftwareSerial WSerial(2, 3);
 
-uint8_t bSwitch = 12, fSwitch = 11, fanOut = 10;
-uint8_t motA = A0, motB = A1;
+uint8_t bSwitch = 12, fSwitch = 11, fanOut = 10,trayButton=9;
+uint8_t motA = A0, motB = A1, led_Alarm=A2,led_Fan=A3,led_Heat=A4,led_Hum=A5,led_cool=4,led1=6,led2=7,led3=8;
 bool forwardMotor = true, startMotor = false, timer2Active = false, timer4Active = false, motorState = false, startTimerTwo = false, startTimerOne = false, startFan = false;
 uint8_t restingPosition = 1;
 
@@ -21,14 +21,25 @@ void setup() {
     pinMode(motA, OUTPUT);
     pinMode(motB, OUTPUT);
     pinMode(fanOut, OUTPUT);
+    pinMode(trayButton,INPUT);
+    pinMode(led_Alarm,OUTPUT);
+    pinMode(led_Fan,OUTPUT);
+    pinMode(led_Heat,OUTPUT);
+    pinMode(led_Hum,OUTPUT);
+    pinMode(led_cool,OUTPUT);
+    pinMode(led1,OUTPUT);
+    pinMode(led2,OUTPUT);
+    pinMode(led3,OUTPUT);
 
     Serial.begin(9600);
     WSerial.begin(9600);
 
     digitalWrite(motA, LOW);
     digitalWrite(motB, LOW);
+   // EEPROM.write(0,1); //to be commented after first run
     restingPosition = EEPROM.read(0);
 }
+
 
 void loop() {
     StaticJsonDocument<1024> doc;
@@ -50,11 +61,13 @@ void loop() {
 
     if (startFan) {
         digitalWrite(fanOut, HIGH);
+        digitalWrite(led_Fan,HIGH);
 
     }
     else {
 
         digitalWrite(fanOut, LOW);
+        digitalWrite(led_Fan,LOW);
     }
 ////TODO: for demo purposes only
 //if(demo.isReady()){
@@ -62,16 +75,16 @@ void loop() {
 //    demo.reset();
 //}
 
-    if (motorState) {
-        startMotor = true;
-    }
+//    if (motorState) {
+//        startMotor = true;
+//    }
 
     if (startMotor == false) {
         digitalWrite(motA, LOW);
         digitalWrite(motB, LOW);
     }
 
-    if (digitalRead(fSwitch) == 0) {
+    if (digitalRead(fSwitch) == 1) {
         forwardMotor = false;
         Serial.println("Forward pressed");
 
@@ -88,7 +101,7 @@ void loop() {
         }
     }
 
-    if (digitalRead(bSwitch) == 0) {
+    if (digitalRead(bSwitch) == 1) {
         forwardMotor = true;
         Serial.println("Backwards pressed");
 
@@ -122,22 +135,23 @@ void loop() {
     }
 
     if (forwardMotor == true && startMotor == true) {
-        digitalWrite(motA, HIGH);
-        digitalWrite(motB, LOW);
+        digitalWrite(motA, LOW);
+        digitalWrite(motB, HIGH);
+
         Serial.println("F===========");
     }
 
     if (forwardMotor == false && startMotor == true) {
-        digitalWrite(motA, LOW);
-        digitalWrite(motB, HIGH);
+        digitalWrite(motA, HIGH);
+        digitalWrite(motB, LOW);
         Serial.println("B===========");
     }
-    Serial.println("****************************");
-    Serial.print("Resting Position: ");
-    Serial.println(restingPosition);
-    Serial.print("start Motor: ");
-    Serial.println(startMotor);
-    Serial.print("forward Motor: ");
-    Serial.println(forwardMotor);
-    Serial.println("****************************");
+//    Serial.println("****************************");
+//    Serial.print("Resting Position: ");
+//    Serial.println(restingPosition);
+//    Serial.print("start Motor: ");
+//    Serial.println(startMotor);
+//    Serial.print("forward Motor: ");
+//    Serial.println(forwardMotor);
+//    Serial.println("****************************");
 }
